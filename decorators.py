5884df50a302c5fa035806944166d028c5ae205c
zakmatt <zak.matthew@yahoo.com>
2017-11-27 23:44:34 -0500

    decorators with parameters

--- decorators.py
+++ decorators.py
@@ -12,5 +12,26 @@ def my_decorator(func):
 def super_function():
     print("I like pancakes")
     
+
+def decorator_with_args(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_which_runs_func(*args, **kwargs):
+            print("Before func.")
+            a = func(*args, **kwargs) + 5
+            print("After func.")
+            return a
+        return function_which_runs_func
+    return my_decorator
+    
+@decorator_with_args(123)
+def super_second_function(x, y):
+    print("I like pancakes too!" )
+    print(x+y)
+    return 5
+    
+    
+    
 if __name__=='__main__':
     super_function()
+    print(super_second_function(3, 23))
